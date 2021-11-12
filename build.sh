2011600b70ecfb841180ee14a799eb11a7b44215
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 12:57:40 -0800

    Refactor

--- build.sh
+++ build.sh
@@ -15,12 +15,21 @@ cd python
 # This sed syntax is GNU sed specific
 [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
 
-${GNU_SED} -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" 3.10/bullseye/Dockerfile
+${GNU_SED} -i \
+    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" \
+    -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" \
+    3.10/bullseye/Dockerfile
 
 # Build
 
-docker build 3.10/bullseye/ --platform linux/amd64 --tag ghcr.io/golden-containers/python:3.10-bullseye --label ${1:-DEBUG=TRUE}
+[ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
+
+BUILD_PLATFORM=" --platform linux/amd64 "
+GCI_URL="ghcr.io/golden-containers"
+BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
+
+docker build 3.10/bullseye/ --tag ${GCI_URL}/python:3.10-bullseye ${BUILD_ARGS}
 
 # Push
 
-docker push ghcr.io/golden-containers/python -a
+docker push ${GCI_URL}/python -a
